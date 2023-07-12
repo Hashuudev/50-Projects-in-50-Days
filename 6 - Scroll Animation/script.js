edb1ccba1e3956c44a998e777a00e777bf0892b7
Hashir Ali <94761963+Hashuudev@users.noreply.github.com>
2023-07-13 01:12:09 +0500

    Add files via upload

---
+++ 6 - Scroll Animation/script.js
@@ -0,0 +1,20 @@
+"use-strict";
+
+const boxes = document.querySelectorAll(".box");
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
+checkBoxes();
+
+window.addEventListener("scroll", checkBoxes);
