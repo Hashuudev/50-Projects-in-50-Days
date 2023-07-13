fb5793ac2448bf5ceab1d53d2d130344330e9bad
Hashir Ali <94761963+Hashuudev@users.noreply.github.com>
2023-07-14 03:13:09 +0500

    Add files via upload

---
+++ 12 - FAQ Collapse/script.js
@@ -0,0 +1,9 @@
+"use-strict";
+
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
