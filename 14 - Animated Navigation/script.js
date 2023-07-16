51c34bba7539cc59a09e072ed630e4da4da16120
Hashir Ali <94761963+Hashuudev@users.noreply.github.com>
2023-07-16 22:47:33 +0500

    Add files via upload

---
+++ 14 - Animated Navigation/script.js
@@ -0,0 +1,8 @@
+"use-strict";
+
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
